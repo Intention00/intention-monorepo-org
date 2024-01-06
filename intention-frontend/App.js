076558fd758c26989abd4da74bfb645c0ea06abb
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-01-06 15:41:16 -0800

    added ability to retrieve contact permission from android
    still need to test if it works on ios

--- intention-frontend/App.js
+++ intention-frontend/App.js
@@ -16,30 +16,30 @@ import Home from './Components/Home/Home';
   // }, []);
 
   function App() {
-    //use state will be used to manage state of the data object 
-    // object for storing and using data
-    const [data, setdata] = useState({
-        name: "",
-        phone: ""
+    // //use state will be used to manage state of the data object 
+    // // object for storing and using data
+    // const [data, setdata] = useState({
+    //     name: "",
+    //     phone: ""
        
-    });
+    // });
  
-    // Using useEffect is used to fetch data once component mounts
-    // it fetches once bc of dependect array [] 
-    useEffect(() => {
-        // Using fetch to fetch the api from 
-        // flask server it will be redirected to proxy
-        fetch("http://127.0.0.1:5100/contact").then((res) =>
-            res.json().then((data) => {
-                // Setting a data from api
-                setdata({
-                    name: data.name,
-                    phone: data.phone,
+    // // Using useEffect is used to fetch data once component mounts
+    // // it fetches once bc of dependect array [] 
+    // useEffect(() => {
+    //     // Using fetch to fetch the api from 
+    //     // flask server it will be redirected to proxy
+    //     fetch("http://127.0.0.1:5100/contact").then((res) =>
+    //         res.json().then((data) => {
+    //             // Setting a data from api
+    //             setdata({
+    //                 name: data.name,
+    //                 phone: data.phone,
                    
-                });
-            })
-        );
-    }, []);
+    //             });
+    //         })
+    //     );
+    // }, []);
 
 
 // need to set upif else statement to render different views whether data is empty or not 
--- intention-frontend/Components/ContactSync/ContactSync.tsx
+++ intention-frontend/Components/ContactSync/ContactSync.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {View, Text} from 'react-native';
 import * as Contacts from 'expo-contacts';
 
@@ -17,6 +17,45 @@ const ContactSync: React.FC = ()=> {
     //         }
     //     })
     // })
+
+    let [error, setError] = useState(undefined);
+    let [contacts, setContacts] = useState(undefined);
+
+    useEffect(()=> {
+        console.log("made inside try block.");
+        (async ()=> {
+            console.log("made inside async func");
+            try {
+                const {status} = await Contacts.requestPermissionsAsync();
+                if (status === 'granted') {
+                    const { data } = await Contacts.getContactsAsync({
+                        fields: [
+                            Contacts.Fields.Birthday,
+                            Contacts.Fields.Emails,
+                            Contacts.Fields.FirstName,
+                            Contacts.Fields.LastName,
+                            Contacts.Fields.PhoneNumbers
+                        ]
+                    });
+
+                    if (data.length > 0) {
+                        console.log(data);
+                        setContacts(data);
+                        
+                    } else {
+                        setError("No contacts available");
+                    }
+                } else {
+                    setError('Permission to access contacts denied.');
+                }
+            }
+            catch (err) {
+                console.log("Error with promise: ", err);
+            }
+            
+        })()
+        
+    }, [])
 
 
     return (
