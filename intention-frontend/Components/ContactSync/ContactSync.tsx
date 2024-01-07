e7a981a3d80343bccbf7c9f0bbf07fb8b9d37322
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-01-06 21:40:40 -0800

    Display contact names/numbers

--- intention-frontend/Components/ContactSync/ContactSync.tsx
+++ intention-frontend/Components/ContactSync/ContactSync.tsx
@@ -4,34 +4,16 @@ import * as Contacts from 'expo-contacts';
 
 const ContactSync: React.FC = ()=> {
 
-    // useEffect(()=>{
-    //     (async ()=> {
-    //         const {status} = await Contacts.requestPermissionsAsync();
-    //         if (status === 'granted') {
-    //             const {data} = await Contacts.getContactsAsync({fields: [Contacts.Fields.Emails],});
-
-    //             if (data.length > 0) {
-    //                 const contact = data[0];
-    //                 console.log(contact);
-    //             }
-    //         }
-    //     })
-    // })
-
     let [error, setError] = useState(undefined);
     let [contacts, setContacts] = useState(undefined);
 
     useEffect(()=> {
-        console.log("made inside try block.");
         (async ()=> {
-            console.log("made inside async func");
             try {
                 const {status} = await Contacts.requestPermissionsAsync();
                 if (status === 'granted') {
                     const { data } = await Contacts.getContactsAsync({
                         fields: [
-                            Contacts.Fields.Birthday,
-                            Contacts.Fields.Emails,
                             Contacts.Fields.FirstName,
                             Contacts.Fields.LastName,
                             Contacts.Fields.PhoneNumbers
@@ -39,7 +21,6 @@ const ContactSync: React.FC = ()=> {
                     });
 
                     if (data.length > 0) {
-                        console.log(data);
                         setContacts(data);
                         
                     } else {
@@ -57,10 +38,39 @@ const ContactSync: React.FC = ()=> {
         
     }, [])
 
+    const getPhoneNumbers = (contact)=> {
+        if (contact.phoneNumbers !== undefined) {
+            return contact.phoneNumbers.map((phoneNumber, index)=> {
+                return (
+                    <View key={index}>
+                        <Text>{phoneNumber.label}: {phoneNumber.number}</Text>
+                    </View>
+                )
+            })
+        }
+    }
+
+    const getContacts = ()=> {
+        if (contacts !== undefined) {
+            return contacts.map((contact, index) => {
+                return (
+                    <View key={index}>
+                        <Text>Name: {contact.firstName} {contact.lastName}</Text>
+                        {getPhoneNumbers(contact)}
+                    </View>
+                );
+            });
+
+        } else {
+            return <Text>Contacts loading...</Text>
+        }
+    }
+
 
     return (
         <View>
-            <Text>Syncing</Text>
+            <Text>{error}</Text>
+            {getContacts()}
         </View>
     );
 };
