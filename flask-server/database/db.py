da0b24e8e834b0e2b1ceca8194612e6cbe7b2291
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-01-18 13:05:45 -0800

    setup basic connection to db with python

---
+++ flask-server/database/db.py
@@ -0,0 +1,68 @@
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+import os
+import mysql.connector
+
+# Connect to the database
+connection = mysql.connector.connect(
+  host=os.getenv("DATABASE_HOST"),
+  user=os.getenv("DATABASE_USERNAME"),
+  passwd=os.getenv("DATABASE_PASSWORD"),
+  db=os.getenv("DATABASE"),
+  autocommit=True,
+  ssl_verify_identity=True,
+  ssl_ca='/etc/ssl/cert.pem'
+)
+
+cursor = connection.cursor(dictionary=True)
+cursor.execute("""
+CREATE TABLE Contact ( 
+
+    ContactID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+
+    FirstName VARCHAR(255) NOT NULL, 
+
+    LastName VARCHAR(255) NOT NULL, 
+
+    UserID INT NOT NULL,
+
+    LastContactedDate DATETIME, 
+
+    ContactFrequency INT, 
+
+    Phone VARCHAR(15), 
+
+    Email VARCHAR(255), 
+
+    BucketID INT NULL
+
+); 
+""")
+
+cursor.close()
+connection.close()
+
+# try:
+#     # Create a cursor to interact with the database
+#     cursor = connection.cursor()
+
+#     # Execute "SHOW TABLES" query
+#     cursor.execute("SHOW TABLES")
+
+#     # Fetch all the rows
+#     tables = cursor.fetchall()
+
+#     # Print out the tables
+#     print("Tables in the database:")
+#     for table in tables:
+#         print(table[0])
+
+# except mysql.connector.Error as e:
+#     print("MySQL Error:", e)
+
+# finally:
+#     # Close the cursor and connection
+#     cursor.close()
+#     connection.close()
---
+++ flask-server/database/schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE User ( 
+    UserID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+
+    Email VARCHAR(255), 
+
+    Password VARCHAR(255) NOT NULL, 
+
+    UserName VARCHAR(255) NOT NULL, 
+
+    PersonType VARCHAR(255)
+); 
+
+
+CREATE TABLE Contact ( 
+
+    ContactID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+
+    FirstName VARCHAR(255) NOT NULL, 
+
+    LastName VARCHAR(255) NOT NULL, 
+
+    UserID INT NOT NULL,
+
+    LastContactedDate DATETIME, 
+
+    ContactFrequency INT, 
+
+    Phone VARCHAR(15), 
+
+    Email VARCHAR(255), 
+
+    BucketID INT NULL
+
+); 
+
+-- INSERTS
+
+INSERT INTO User (Email, Password, UserName, PersonType) VALUES 
+
+('user1@example.com', 'pass123', 'UserOne', 'Admin');
+
+INSERT INTO Contact (FirstName, LastName, UserID, LastContactedDate, ContactFrequency, Phone, Email, BucketID) VALUES 
+
+('John', 'Doe', 1, '2024-01-01 06:24:59', 5, '555-1234', 'johndoe@example.com', 1);
