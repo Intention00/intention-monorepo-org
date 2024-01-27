2c714cb639c9f184a9a9ffb079a5394ca8142dd8
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-01-27 15:41:47 -0800

    cleaning up syntax a bit

--- intention-frontend/Components/Transcriber/TranscribeNote.tsx
+++ intention-frontend/Components/Transcriber/TranscribeNote.tsx
@@ -12,21 +12,22 @@ const TranscriberNote: React.FC = ()=> {
 
     async function startRecording() {
         try {
-        if (permissionResponse.status !== 'granted') {
-            console.log('Requesting permission..');
-            await requestPermission();
-        }
-        await Audio.setAudioModeAsync({
-            allowsRecordingIOS: true,
-            playsInSilentModeIOS: true,
-        });
+            if (permissionResponse.status !== 'granted') {
+                console.log('Requesting permission..');
+                await requestPermission();
+            }
+            await Audio.setAudioModeAsync({
+                allowsRecordingIOS: true,
+                playsInSilentModeIOS: true,
+            });
 
-        console.log('Starting recording..');
-        const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY);
-        setRecording(recording);
-        console.log('Recording started');
-        } catch (err) {
-        console.error('Failed to start recording', err);
+            console.log('Starting recording..');
+            const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY);
+            setRecording(recording);
+            console.log('Recording started');
+        } 
+        catch (err) {
+            console.error('Failed to start recording', err);
         }
     }
 
@@ -34,11 +35,8 @@ const TranscriberNote: React.FC = ()=> {
         console.log('Stopping recording..');
         setRecording(undefined);
         await recording.stopAndUnloadAsync();
-        await Audio.setAudioModeAsync(
-        {
-            allowsRecordingIOS: false,
-        }
-        );
+        await Audio.setAudioModeAsync({ 
+            allowsRecordingIOS: false,});
         const uri = recording.getURI();
         console.log('Recording stopped and stored at', uri);
         setAudioUri(uri);
@@ -66,12 +64,10 @@ const TranscriberNote: React.FC = ()=> {
     }
 
     useEffect(() => {
-        return sound
-        ? () => {
+        return sound ? () => {
             console.log('Unloading Sound');
             sound.unloadAsync();
-            }
-        : undefined;
+        } : undefined;
     }, [sound]);
 
     // audio test end
