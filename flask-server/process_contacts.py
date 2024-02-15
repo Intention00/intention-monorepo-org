1ea93fc37592f2a8b6aca9a9549f00009a3ff735
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-02-15 00:02:05 -0800

    fixed context provider issue and duplicate contact entries

--- flask-server/process_contacts.py
+++ flask-server/process_contacts.py
@@ -42,9 +42,31 @@ class ProcessContacts():
     # work in progress to insert in contacts that aren't already in the database
     def sync_contacts(self, user_id):
         with DBConnection() as db_conn:
+            # if db_conn:
+            #     sql_statement = """
+            #         INSERT IGNORE INTO Contact (FirstName, LastName, UserID, Phone) VALUES (%s, %s, %s, %s);
+            #     """
+            #     for contact in self.contacts:
+            #         db_conn.execute(sql_statement, (contact['firstName'], contact['lastName'], user_id, contact['number']))
             if db_conn:
-                sql_statement = """
-                    INSERT IGNORE INTO Contact (FirstName, LastName, UserID, Phone) VALUES (%s, %s, %s, %s);
-                """
+                # check each contact
                 for contact in self.contacts:
-                    db_conn.execute(sql_statement, (contact['firstName'], contact['lastName'], user_id, contact['number']))
+
+                    # check for duplicates
+                    sql_statement = """
+                        SELECT * FROM Contact WHERE UserID = %s AND Phone = %s;
+                    """
+                    db_conn.execute(sql_statement, (user_id, contact['number']))
+                    existing_contact = db_conn.fetchone()
+
+                    # if duplicate, then don't add to db, might need to add update logic later
+                    if existing_contact:
+                        # do nothing
+                        print(f"Contact called: {self.contacts['firstName']} {self.contacts['lastName']} already exists!")
+                    
+                    # if not duplicate, insert to db
+                    else:
+                        sql_statement = """
+                            INSERT INTO Contact (FirstName, LastName, UserID, Phone) VALUES (%s, %s, %s, %s);
+                        """
+                        db_conn.execute(sql_statement, (contact['firstName'], contact['lastName'], user_id, contact['number']))
