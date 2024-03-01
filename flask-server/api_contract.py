ed867f011226e0af01a4efd12352ba7e9f8c4eee
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-02-29 19:24:16 -0800

    created some sample api contracts

---
+++ flask-server/api_contract.py
@@ -0,0 +1,21 @@
+'''
+DELETE /api/users/:id
+
+Input parameters:
+- id (int): The userid of the person whose data we want to delete from DB
+- full (bool): Whether we want a full deletion of the user, or only all data except account
+
+Output:
+- type (string): "Full" or "Partial"
+- msg (string): "User was successfully deleted"
+
+Example usage:
+
+DELETE /api/users/22?full=true
+
+Output:
+{
+    "type": "Full",
+    "msg": "User was successfully deleted"
+}
+'''
