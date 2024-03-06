b524181b9361ffd534895b3b9c0c84d7a146993d
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-03-06 13:54:10 -0800

    created delete user data and user buttons on profile page
    they seem to work with manual testing

--- intention-frontend/app/(auth)/Components/ContactSync/backendService.ts
+++ intention-frontend/app/(auth)/Components/ContactSync/backendService.ts
@@ -1,5 +1,5 @@
 // export const backendAddress = "http://192.168.1.27:5100"
-const backendAddress = "http://72.233.177.86:5100"
+export const backendAddress = "http://72.233.177.86:5100"
   // const backendAddress = "http://127.0.0.1:5100"
 //   const backendAddress = "http://10.0.0.5:5100"
 
