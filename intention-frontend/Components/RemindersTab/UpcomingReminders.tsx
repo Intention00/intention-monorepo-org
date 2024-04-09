e27b843c6105063abc291699dcc029d35008a959
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-04-09 12:26:58 -0700

    added tab and root component for reminders tab

---
+++ intention-frontend/Components/RemindersTab/UpcomingReminders.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+const UpcomingReminders: React.FC = ()=> {
+    return (
+        <View>
+            <Text>REMINDERS!</Text>
+        </View>
+    )
+}
+
+export {UpcomingReminders};
