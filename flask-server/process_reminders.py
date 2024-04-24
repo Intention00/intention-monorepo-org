ef0cb951d5995a73fce106a8d98e5cb4863fe1f3
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-04-23 21:18:24 -0700

    changed retrieve reminders to send real data back instead of only dummy data

--- flask-server/process_reminders.py
+++ flask-server/process_reminders.py
@@ -65,82 +65,32 @@ class ProcessReminders():
     def retrieve_reminders(self):
         with DBConnection() as db_conn:
             if db_conn:
-                # sql_statement = """
-                #     SELECT * FROM Reminders WHERE UserID = %s;
-                # """
-                # db_conn.execute(sql_statement, (self.user_id,))
-                # db_info = db_conn.fetchall()
+                sql_statement = """
+                    SELECT c.ContactID, c.FirstName, c.LastName, c.Phone, r.StartDateTime, r.Frequency 
+                    FROM Reminders AS r INNER JOIN Contact AS c ON 
+                    r.ContactID = c.ContactID WHERE UserID = %s;
+                """
+                db_conn.execute(sql_statement, (self.user_id,))
+                db_reminders = db_conn.fetchall()
 
-                # print(db_info);
-
-                return [
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Month', 'lastName': 'Week1', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-16 21:30:00', 'frequency': 'monthly'}
+                try:
+                    # formatting the desired reminders the way expected by the frontend
+                    formatted_reminders = [
+                        {
+                            'contact': {
+                                'contactID': reminder['ContactID'],
+                                'firstName': reminder['FirstName'],
+                                'lastName': reminder['LastName'],
+                                'number': reminder['Phone']
                             },
-
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Month', 'lastName': 'Week2', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-16 16:30:00', 'frequency': 'monthly'}
-                            },
-
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Month', 'lastName': 'Week3', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-03-17 19:30:00', 'frequency': 'monthly'}
-                            },
-
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Month', 'lastName': 'Week4', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-19 19:30:00', 'frequency': 'monthly'}
-                            },
-
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Jessica', 'lastName': 'Blake', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-14 18:30:00', 'frequency': 'daily'}
-                            },
-
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Tristine', 'lastName': 'Timely', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-14 20:40:00', 'frequency': 'daily'}
-                            },
-
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Torrik', 'lastName': 'Mornin', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-14 11:59:59', 'frequency': 'daily'}
-                            },
-
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Adam', 'lastName': 'Berk', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-14 19:45:00', 'frequency': 'monthly'}
-                            },
-
-                            {
-                                'contact': {'contactID': -1, 'firstName': 'Ted', 'lastName': 'Kline', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-14 12:50:00', 'frequency': 'weekly'}
-                            }, 
-
-                            {
-                                'contact': {'contactID': -1, 'firstName': 'Bob', 'lastName': 'Iger', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-07 12:45:00', 'frequency': 'weekly'}
-                            }, 
-
-                            {
-                                'contact': {'contactID': -2, 'firstName': 'Alex', 'lastName': 'Martelli', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-14 14:00:00', 'frequency': 'monthly'}
-                            }, 
-
-                            {
-                                'contact': {'contactID': -1, 'firstName': 'Weekly', 'lastName': 'Repeat', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-8 23:59:00', 'frequency': 'weekly'}
-                            }, 
-
-                            {
-                                'contact': {'contactID': -1, 'firstName': 'MaxLength', 'lastName': 'Test', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-14 23:59:00', 'frequency': 'weekly'}
-                            }, 
-
-                            {
-                                'contact': {'contactID': -1, 'firstName': 'MaxLength', 'lastName': 'Test2', 'number': '0000000000'}, 
-                                'reminder': {'dateTime': '2024-04-14 23:59:00', 'frequency': 'weekly'}
-                            }, 
-                        ]
+                            'reminder': {
+                                'dateTime': reminder['StartDateTime'],
+                                'frequency': reminder['Frequency']
+                            }
+                        } for reminder in db_reminders
+                    ]
+                    return formatted_reminders
+                
+                except Exception as err:
+                    print(f'Reminders retrieval failed: {err}')
+                    return None
