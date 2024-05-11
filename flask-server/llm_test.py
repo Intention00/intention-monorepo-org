b1f3562503a1d5312d32558130faf57c42972d89
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-05-11 00:16:53 -0700

    added some more logic to test speeds

--- flask-server/llm_test.py
+++ flask-server/llm_test.py
@@ -6,6 +6,7 @@
 from process_notes import ProcessNotes
 import time
 import os
+import json
 
 gpt_processor = ProcessNotes(model_name='gpt')
 llama_processor = ProcessNotes(model_name='llama3')
@@ -15,8 +16,12 @@ def measure_time(processor: ProcessNotes):
     output = processor.get_summary_questions(6, "Hank")
     end_time = time.time()
     total_time = end_time - start_time
-    result = f"Model: {processor.model_name}\nOutput: {output}\nTime: {total_time: .2f}\n"
-    print(result)
+    print(f"Model: {processor.model_name}\nOutput: {output}\nTime: {total_time: .2f}\n")
+    result = {
+        'model': processor.model_name,
+        'output': output,
+        'time': total_time
+    }
     return total_time, result
 
 def record_time(n = 1):
@@ -28,10 +33,17 @@ def record_time(n = 1):
         for i in range(n):
             gpt_time, gpt_result = measure_time(gpt_processor)
             llama_time, llama_result = measure_time(llama_processor)
+
+            gpt_result['iteration'] = i
+            llama_result['iteration'] = i
+
             gpt_times.append(gpt_time)
             llama_times.append(llama_time)
-            file.write(gpt_result)
-            file.write(llama_result)
+            # file.write(gpt_result)
+            # file.write(llama_result)
+            json.dump(gpt_result, file)
+            json.dump(llama_result, file)
+
 
         print(f'GPT times: {gpt_times}')
         print(f'Llama times: {llama_times}')
