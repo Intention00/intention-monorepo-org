5bce48084f00e9ece2e3755e7ded0c3179da96d0
mikhaelsoebrotoSPU <soebrotom@spu.edu>
2024-05-15 13:00:02 -0700

    Work in Progress Styling tags

--- intention-frontend/Components/ContactsTab/ContactsTagging/tag.style.tsx
+++ intention-frontend/Components/ContactsTab/ContactsTagging/tag.style.tsx
@@ -7,20 +7,24 @@ import { styles as global } from '../../Generic/global.style';
 
  const styles = StyleSheet.create({
     tagContainer: {
-        flexDirection: 'row', // Ensure that the tag and the close button are aligned horizontally
+        flexDirection: 'column', // Ensure that the tag and the close button are aligned horizontally
         alignItems: 'center', // Align items vertically within the container
       },
     tag: {
-    backgroundColor: global.tagBackground.color,
-    paddingVertical: 5,
+    backgroundColor: global.accentColor.color,
+    width: 'auto',
+    paddingVertical: 10,
     paddingHorizontal: 10,
-    borderRadius: 10,
-    marginRight: 10,
+    borderRadius: 5,
+    marginRight: 5,
+    marginLeft: 5,
     marginVertical: 5,
   },
   tagText: {
-    color: global.tagText.color,
-    fontSize: 12,
+    color: global.buttonText.color,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
   },
 });
 
