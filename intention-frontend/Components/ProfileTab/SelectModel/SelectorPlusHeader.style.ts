02db524ed2f88c0169254200dc64b01663c05e76
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-05-17 18:02:19 -0700

    last update

--- intention-frontend/Components/ProfileTab/SelectModel/SelectorPlusHeader.style.ts
+++ intention-frontend/Components/ProfileTab/SelectModel/SelectorPlusHeader.style.ts
@@ -1,12 +1,18 @@
 import { StyleSheet } from "react-native";
 import { styles as global } from "../../Generic/global.style";
 
-
 const styles = StyleSheet.create({
     selectorText: {
-        color: "white", 
+        color: global.bodyText.color, 
         textAlign: 'center',
         marginBottom: 10
+    },
+
+    settingsText: {
+        color: global.accentColor.color,
+        marginBottom: 20,
+        fontWeight: 'bold',
+        fontSize: 16,
     }
 
 })
