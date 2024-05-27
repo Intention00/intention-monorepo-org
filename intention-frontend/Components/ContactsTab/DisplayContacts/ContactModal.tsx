31ec1649ea95af4345e0dc8f3aa0ffac238575bc
mikhaelsoebrotoSPU <soebrotom@spu.edu>
2024-05-27 08:33:13 -0700

    Tags refreshes after adding or deleting

--- intention-frontend/Components/ContactsTab/DisplayContacts/ContactModal.tsx
+++ intention-frontend/Components/ContactsTab/DisplayContacts/ContactModal.tsx
@@ -1,24 +1,30 @@
-import { View, Text, Button,ScrollView, TouchableHighlight } from "react-native"
+import { View, Text, Button,ScrollView, TouchableHighlight, TouchableOpacity } from "react-native"
 import {TranscriberNote} from '../Transcriber/TranscribeNote'
-import React, {useState} from "react";
+import React, {createContext, useState} from "react";
 import { Image } from 'expo-image'
 import { styles } from "./ContactModal.style";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { ContactTags } from '../../ContactsTab/ContactsTagging/ContactTags'
-
+import { ContactItem } from "./ContactItem";
+import { tagUpdater } from "../UserSync/tagUpdater";
 
 const ContactModal: React.FC <{contact, toggleModalVisibility}> = ({contact, toggleModalVisibility})=> {
     const [currentStep, setCurrentStep] = useState("transcriber"); // Default to transcriber
     // const handleRecordingComplete = () => {
     //     setCurrentStep("AI_Generations");
     // };
+    if (tagUpdater.displayName === "true") {
+        tagUpdater.displayName = "false"
+        console.log(tagUpdater);
+    }
+    
     const blurhash = 
     '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
     return (
         <View style={[styles.centeredView, styles.modalView]}>
             <View style={[styles.modalBox]}>
                 <View style={styles.modalHeader}>
-                    <MaterialCommunityIcons style={styles.modalExit} name="window-close" onPress={toggleModalVisibility}/>
+                    <MaterialCommunityIcons style={styles.modalExit} name="window-close" onPress={toggleModalVisibility} onPressIn={() => tagUpdater.displayName= "true"} />
                     <View style={{flex: 1}}>
                         <Text style={styles.modalHeaderText}>Contact Details</Text>
                     </View>
