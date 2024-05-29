fe25f8bc9f838e499d3c067492d7547214f6f6bb
mikhaelsoebrotoSPU <soebrotom@spu.edu>
2024-05-28 17:36:07 -0700

    Logout Modal

--- intention-frontend/app/(auth)/_layout.tsx
+++ intention-frontend/app/(auth)/_layout.tsx
@@ -1,10 +1,15 @@
 import { Tabs } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { View, Text, Button, ScrollView, TouchableOpacity, Modal, TextInput, Image } from 'react-native';
 import { Pressable } from 'react-native';
 import { useAuth } from '@clerk/clerk-expo';
 import { styles as global } from '../../Components/Generic/global.style';
+import { useState } from 'react';
+import { styles } from '../../Components/ContactsTab/ContactsTagging/ContactTags.style'
 
 export const LogoutButton = () => {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  
   const { signOut } = useAuth();
 
   const doLogout = () => {
@@ -12,9 +17,54 @@ export const LogoutButton = () => {
   };
 
   return (
-    <Pressable onPress={doLogout} style={{ marginRight: 10 }}>
-      <Ionicons name="log-out-outline" size={24} color={global.bodyText.color} />
-    </Pressable>
+    <View>
+      <Pressable onPress={() => setIsModalVisible(true)} style={{ marginRight: 10 }}>
+        <Ionicons name="log-out-outline" size={24} color={global.bodyText.color} />
+      </Pressable>
+
+      <Modal
+        visible={isModalVisible}
+        animationType="slide"
+        transparent={true}
+      >
+          <View style={styles.modalCenter}>
+            <View style={styles.modalView}>
+              <View style={{paddingTop: 10,
+                marginBottom: 10,
+                flexDirection: 'row',
+                alignItems: 'center'}}>
+                <Ionicons name="log-out-outline" size={130} color={global.bodyText.color} style={{paddingLeft: '35%'}} />
+                {/* <View style={{flex: 1}}>
+                  <Text style={styles.modalHeaderText}>SignOut</Text>
+                </View> */}
+              </View>
+              <View style={{paddingLeft:15}}>
+                  <Text style={{color: '#FFF', fontWeight: 'bold', textAlign: 'center', fontSize: 16}}> Oh No, Are you Sure You want to Sign Out of this Account?</Text>
+              </View>
+              <View style={{alignSelf: 'flex-end', paddingTop: "15%",flexDirection:'row', }}>
+                  <View style={{paddingRight: "3%"}}>
+                  <TouchableOpacity style={{
+                    backgroundColor: styles.modalButton.backgroundColor,
+                    borderRadius: 5,
+                    padding: 10,
+                    width: "auto"}} onPress={() => setIsModalVisible(false)}>
+                    <Text style={{fontSize:16, color:global.bodyText.color}}>Cancel</Text>
+                  </TouchableOpacity>
+                  </View>
+                  <TouchableOpacity style={{
+                    backgroundColor: global.accentColor.color,
+                    borderRadius: 5,
+                    padding: 10,
+                    width: "auto"}} onPress={doLogout}>
+                    <Text style={{fontSize:16, color:global.buttonText.color}}>Yes, Sign Me Out</Text>
+                  </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+      </Modal>
+
+    </View>
+    
   );
 };
 
